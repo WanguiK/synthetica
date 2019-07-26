f4acbc20f93e24acdb3f6831f1df4ec624268c6c
Wangui Kamande <kamande@leicunnstudio.com>
2019-07-26 04:18:36 +0300

    Data generates but before formatting table

---
+++ FinalProject/Synthetica/artificial.py
@@ -0,0 +1,66 @@
+import logging
+import random
+import pandas as pd
+from tabulate import tabulate
+
+from .trumania.trumania.core import util_functions as util_functions
+from .trumania.trumania.core import circus
+from .trumania.trumania.core.random_generators import SequencialGenerator, NumpyRandomGenerator
+
+
+util_functions.setup_logging()
+
+logging.info("building student dataset")
+
+
+randint = ['age', 'medu', 'fedu', 'famrel', 'traveltime', 'studytime', 'failures', 'freetime', 'walc', 'dalc', 'health', 'absences', 'g1', 'g2', 'g3']
+choices = ['sex', 'address', 'pstatus', 'mjob', 'fjob','guardian', 'famsize', 'reason', 'schoolsup', 'famsup','activities', 'paidclass', 'internet']
+
+def build_circus():
+    seed = random.randrange(1, 1000)
+    return circus.Circus(
+        name="Student Dataset",
+        master_seed=seed,
+        start=pd.Timestamp("1 Jan 2017 00:00"),
+        step_duration=pd.Timedelta("1h")
+    )
+
+
+def create_student_population(the_circus, size, metadata = []):    
+    student = the_circus.create_population (
+        name = "Student Information", 
+        size = size,
+        ids_gen = SequencialGenerator()
+    )
+    length = len(metadata)
+    for i in range(length):
+        if metadata[i][1] in choices:
+            opt = metadata[i][2].split (",")
+            name = 'gen' + str(i)
+            name = NumpyRandomGenerator(
+                method = "choice",
+                a = opt,
+                seed = next(the_circus.seeder)
+            )
+            student.create_attribute(metadata[i][0], init_gen = name)
+        elif metadata[i][1] in randint:
+            opt = metadata[i][2].split (",")
+            name = 'gen' + str(i)
+            name = NumpyRandomGenerator(
+                method = "randint",
+                low = min(opt), 
+                high = max(opt),
+                seed = next(the_circus.seeder)
+            )
+            student.create_attribute(metadata[i][0], init_gen = name)
+    table = tabulate (
+            student.to_dataframe(),
+            headers='keys', 
+            tablefmt='html'
+    )
+    return table
+
+def generate(s, m):
+    dataset_circus = build_circus()
+    table = create_student_population(dataset_circus, s, m)
+    return table
---
+++ FinalProject/Synthetica/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render
+from django.views import generic
+from django.views.generic.edit import CreateView
+from django.db import transaction
+from django.shortcuts import get_object_or_404
+
+from .models import *
+from .forms import *
+from . import artificial
+
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+class ProjectDetailView(generic.DetailView):
+    model = Project
+
+
+class GenerateDetailView(generic.DetailView):
+    model = Generate
+
+
+class ProjectGenerateCreate(CreateView):
+    model = Project
+    fields = ['project_name', 'description', 'records']
+
+    def get_context_data(self, **kwargs):
+        data = super(ProjectGenerateCreate, self).get_context_data(**kwargs)
+        if self.request.POST:
+            data['generate'] = GenerateFormSet(self.request.POST)
+        else:
+            data['generate'] = GenerateFormSet()
+        return data
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        generate = context['generate']
+        with transaction.atomic():
+            self.object = form.save()
+            if generate.is_valid():
+                generate.instance = self.object
+                generate.save()
+        return super(ProjectGenerateCreate, self).form_valid(form)
+
+
+def generate(request, pk):
+    project_instance = get_object_or_404(Project, pk=pk)
+    records = project_instance.records
+    where = Generate.objects.filter(project_id = project_instance).values_list('field_name', 'data_type', 'options')
+    field_data = list(where)
+    meta_data = []
+    for val in field_data: 
+        convert = list(val)
+        meta_data.append(convert)
+    syn = artificial.generate(records, meta_data)
+    context = {
+        'showtable':syn
+    }
+    return render(request, 'generation.html', context)
